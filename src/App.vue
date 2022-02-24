e479dd5826dee55bfa852d7fa9daaa030987a2d1
xuyingfa <3293232930@qq.com>
2022-02-24 20:48:25 +0800

    add vite

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
